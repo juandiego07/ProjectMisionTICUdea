5459ce0a35fc9a1a74e5b295f6881445985cb8dd
juandiego07 <juan_diego07@hotmail.com>
2021-10-12 12:05:06 -0500

    Se retira archivo Js de validación y se ajustan estilos del login

--- banco-del-estado-frontend/src/components/Layout/Home.jsx
+++ banco-del-estado-frontend/src/components/Layout/Home.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import "./Style.css"
 
 export default function Home() {
     return (
--- banco-del-estado-frontend/src/components/Login/index.jsx
+++ banco-del-estado-frontend/src/components/Login/index.jsx
@@ -1,27 +1,36 @@
+import "./Style.css";
 export default function Login() {
-
   return (
-    <div className="contenedor">
-      <div className="imagenBanco">
-        <span className="material-icons pe-2">account_balance</span>
-        <span>Banco del Estado</span>
-      </div>
-      <div>
-        <form id="formulario" action="/ventas">
-          <input className="form-control" type="text" placeholder="Usuario" />
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Contraseña"
-          />
-          <div className="divBotonLogin">
-            <button
-            id="btnIngresar"
-            type="submit"
-              className="btn boton"
-            >Ingresar con google</button>
-          </div>
-        </form>
+    <div className="contenedor container-fluid">
+      <div className="row">
+        <div className="imagenBanco col-8 col-sm-6 col-md-5 col-lg-3 m-auto">
+          <span className="material-icons pe-2">account_balance</span>
+          <span>Banco del Estado</span>
+        </div>
+        <div>
+          <form id="formulario" action="/ventas">
+            <div className="col-8 col-sm-6 col-md-5 col-lg-3 m-auto">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Usuario"
+              />
+            </div>
+            <div className="col-8 col-sm-6 col-md-5 col-lg-3 m-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Contraseña"
+              />
+            </div>
+
+            <div className="divBotonLogin col-5 col-sm-4 col-md-3 col-lg-2 m-auto mt-3">
+              <button id="btnIngresar" type="submit" className="btn boton ">
+                Ingresar con google
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
